5a90a456b4d175ef788e795e74e1a8dd6728daae
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-08-02 17:41:52 -0400

    run

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,28 @@ from app.extensions import db, bcrypt, jwt
 from app.api import api_bp
 import config
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to HBnB!'
+
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize extensions
+
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    # Register Blueprint
     app.register_blueprint(api_bp)
 
     return app
